7f987cbf90f7d0b2ec29eed5ddc2a3068994417c
Viktor Rocha <viktoorrocha@gmail.com>
2017-10-23 00:00:40 -0400

    String Templates

---
+++ string_template/palavras.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    var palavra = "pneumoultramicroscopicossilicovulcanoconiótico"
+    println ("A palavra é  $palavra")
+    println("A palavra $palavra possui ${palavra.length} caracteres")
+    val msg = "A maior palavra do mundo é $palavra"
+    println (msg)
+}
+
+/*
+A palavra é pneumoultramicroscopicossilicovulcanoconiótico
+A pneumoultramicroscopicossilicovulcanoconiótico possui 46 caracteres
+A maior palavra do mundo é pneumoultramicroscopicossilicovulcanoconiótico
+*/
---
+++ string_template/string_template.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+var nome = "Viktor"
+println("Olá, $nome")
+println("O nome $nome possui ${nome.length} caracteres")
+val msg = "Meu nome é  $nome"
+val (msg)
+}
+
+/* Olá, Viktor
+   O nome Viktor possui 6 caracteres 
+   Meu nome é Viktor
+   */
