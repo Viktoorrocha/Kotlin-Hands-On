96356ceb79326fb6f42c619b7523748ed5f98209
Viktor Rocha <viktoorrocha@gmail.com>
2017-10-30 12:52:01 -0400

    Up Funcoes Varargs

--- funcoes_varargs/example.kt
+++ funcoes_varargs/example.kt
@@ -1,6 +1,13 @@
-fun toList
-
-
-fun main (args: Array<String>){
-
+fun toList(vararg args: String): List<String>{
+    var list = ArrayList<String>()
+    for (s in args)
+    list.add(s)
+    return list
 }
+fun main (args: Array<String>){
+    var list = toList("Viktor","Rocha","Dev")
+    println(list)
+}
+
+//[Viktor, Rocha, Dev]
+
