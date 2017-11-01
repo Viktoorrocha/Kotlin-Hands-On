24fb3a0ed245380fc93cb3074239edbdd440c804
Viktor Rocha <viktoorrocha@gmail.com>
2017-11-01 17:41:27 -0400

    Listas

---
+++ Listas/example.kt
@@ -0,0 +1,8 @@
+//Podemos utilizar um ArrayList, que eé uma lista ordenada.
+
+val list = ArrayList<String>()
+
+//Porém, se você quiser que o Kotlin crie esta lista para você, é possivel utilizar a função mutableListOf(),
+//que retorna uma lista mutável, ou seja, que pode ser alterada.
+
+val nomes = mutableListOf<Nomes>()
